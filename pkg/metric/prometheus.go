00bc34286c6182f1ab4c67ef069b34759a13a003
Mostafa Oraei <taha.oraei@gmail.com>
2023-11-21 13:47:48 +0330

    response time histogram metric prometheus

--- pkg/metric/prometheus.go
+++ pkg/metric/prometheus.go
@@ -2,11 +2,10 @@ package metric
 
 import (
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 var (
-	ResponseHistogram = promauto.NewHistogram(prometheus.HistogramOpts{
+	ResponseHistogram = prometheus.NewHistogram(prometheus.HistogramOpts{
 		Name:    "eta_response_duration_seconds",
 		Help:    "Duration of ETA responses in seconds",
 		Buckets: prometheus.LinearBuckets(0.001, 0.01, 100), // Modify buckets as per your needs
