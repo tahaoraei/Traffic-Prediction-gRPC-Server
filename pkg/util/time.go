9930717c4727af52a48a186f69503a196c38a821
Mostafa Oraei <taha.oraei@gmail.com>
2023-12-14 12:35:27 +0330

    add time tools for time zone and weekday

--- pkg/util/time.go
+++ pkg/util/time.go
@@ -1,1 +1,25 @@
 package util
+
+import (
+	"time"
+	_ "time/tzdata"
+	"timeMachine/pkg/logger"
+)
+
+var log = logger.Get()
+
+func Now(tz string) string {
+	loc, err := time.LoadLocation(tz)
+	if err != nil {
+		log.Fatal().Msgf("can't find location timezone ", err)
+	}
+	return time.Now().In(loc).Format("2006-01-02 15:04:05")
+}
+
+func Weekday(tz string) int {
+	loc, err := time.LoadLocation(tz)
+	if err != nil {
+		log.Fatal().Msgf("can't find location timezone ", err)
+	}
+	return int(time.Now().In(loc).Weekday())
+}
