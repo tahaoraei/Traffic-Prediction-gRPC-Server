be62ad99380c30697c9efadc895f714197adbb91
Mostafa Oraei <taha.oraei@gmail.com>
2023-12-14 12:37:03 +0330

    move some logic from service to ml and add city and time logic from grpc to here

--- service/timeservice/service.go
+++ service/timeservice/service.go
@@ -3,47 +3,62 @@ package timeservice
 import (
 	"github.com/rs/zerolog/log"
 	"sync"
+	"timeMachine/ml"
 	"timeMachine/param"
+)
+
+const (
+	minXTehran  = 5683551
+	minYTehran  = 4216113
+	maxXTehran  = 5774664
+	maxYTehran  = 4277874
+	minXMashhad = 6583521
+	minYMashhad = 4309213
+	maxXMashhad = 6656388
+	maxYMashhad = 4383510
 )
 
 type Repository interface {
 	GetTrafficLength(zone int8) (int32, error)
 }
 
-type ML interface {
-	GetETAFromML(req *param.ETARequest) *param.ETAResponse
-}
+//type ML interface {
+//	GetETAFromML(req *param.ETARequest) *param.ETAResponse
+//}
 
 type Service struct {
-	repo          Repository
-	ml            ML
-	trafficLength int32
-	zone          int8
+	tehranML  *ml.ML
+	mashhadML *ml.ML
 }
 
-func New(repo Repository, ml ML, zone int8) Service {
-	return Service{repo: repo, ml: ml, zone: zone}
-}
-
-func (s *Service) SetTrafficLength(zone int8) error {
-	l, e := s.repo.GetTrafficLength(zone)
-	if e != nil {
-		log.Warn().Msgf("error in getting traffic length: %s", e.Error())
-		return e
-	}
-	s.trafficLength = l
-	return nil
+func New(tehranML *ml.ML, mashhadML *ml.ML) Service {
+	return Service{tehranML: tehranML, mashhadML: mashhadML}
 }
 
 func (s *Service) GetETA(req *param.ETARequest) *param.ETAResponse {
-	req.TrafficLength = s.trafficLength
-
 	var eta *param.ETAResponse
 	var wg sync.WaitGroup
 
 	processRequest := func() {
 		defer wg.Done()
-		eta = s.ml.GetETAFromML(req)
+
+		if req.Sx > minXTehran && req.Sy > minYTehran && req.Sx < maxXTehran && req.Sy < maxYTehran &&
+			req.Dy > minXTehran && req.Dy > minYTehran && req.Dx < maxXTehran && req.Dy < maxYTehran {
+			if req.Time < 360 || req.CurrentETA < 500 {
+				eta = &param.ETAResponse{ETA: req.CurrentETA}
+			} else {
+				eta = s.tehranML.GetETAFromML(req)
+			}
+		} else if req.Sx > minXMashhad && req.Sy > minYMashhad && req.Sx < maxXMashhad && req.Sy < maxYMashhad &&
+			req.Dx > minXMashhad && req.Dy > minYMashhad && req.Dx < maxXMashhad && req.Dy < maxYMashhad {
+			if req.Time < 390 || req.CurrentETA < 500 {
+				eta = &param.ETAResponse{ETA: req.CurrentETA}
+			} else {
+				eta = s.mashhadML.GetETAFromML(req)
+			}
+		} else {
+			eta = &param.ETAResponse{ETA: req.CurrentETA}
+		}
 	}
 
 	wg.Add(1)
