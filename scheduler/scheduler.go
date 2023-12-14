1e2806fc1af7715c336a79fe08ddf4adb5e9f910
Mostafa Oraei <taha.oraei@gmail.com>
2023-12-14 12:34:50 +0330

    add model changer scheduler base on weekday

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -4,28 +4,43 @@ import (
 	"github.com/go-co-op/gocron"
 	"sync"
 	"time"
-	"timeMachine/service/timeservice"
+	_ "time/tzdata"
+	"timeMachine/ml"
+	"timeMachine/pkg/logger"
 )
 
+var log = logger.Get()
+
 type Scheduler struct {
-	sch        *gocron.Scheduler
-	tehranSvc  *timeservice.Service
-	mashhadSvc *timeservice.Service
+	sch       *gocron.Scheduler
+	tehranML  *ml.ML
+	mashhadML *ml.ML
 }
 
-func New(tehranSvc *timeservice.Service, mashhadSvc *timeservice.Service) Scheduler {
+func New(tehranML *ml.ML, mashhadML *ml.ML) Scheduler {
 	return Scheduler{
-		sch:        gocron.NewScheduler(time.UTC),
-		tehranSvc:  tehranSvc,
-		mashhadSvc: mashhadSvc,
+		sch:       gocron.NewScheduler(time.UTC),
+		tehranML:  tehranML,
+		mashhadML: mashhadML,
 	}
 }
 
 func (s Scheduler) Start(done <-chan bool, wg *sync.WaitGroup) {
 	wg.Done()
 
-	s.sch.Every(30).Second().Do(s.tehranSvc.SetTrafficLength(1))
-	s.sch.Every(30).Second().Do(s.mashhadSvc.SetTrafficLength(2))
+	s.sch.Every(30).Second().Do(func() {
+		s.tehranML.SetTrafficLength(1)
+	})
+	s.sch.Every(30).Second().Do(func() {
+		s.mashhadML.SetTrafficLength(2)
+	})
+	s.sch.Every(3).Hour().Do(func() {
+		s.tehranML.SetNewModel()
+	})
+	s.sch.Every(3).Hour().Do(func() {
+		s.mashhadML.SetNewModel()
+	})
+
 	s.sch.StartAsync()
 
 	<-done
